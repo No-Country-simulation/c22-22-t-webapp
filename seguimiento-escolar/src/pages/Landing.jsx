9d8f002c98d084b77ad278cf73cd2bf00d36598f
Leonardo Fuentes Claros <leonardofuentesclaros@gmail.com>
2024-11-20 00:21:03 -0400

    Set up react project

--- seguimiento-escolar/src/pages/Landing.jsx
+++ seguimiento-escolar/src/pages/Landing.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 function Landing() {
   return (
-    <div>Landing</div>
+    <div>Landing
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus unde recusandae, iusto quaerat aliquam repellendus totam dolor ipsam earum quia, molestias dolores est quas nostrum maxime. Doloribus eum dolor repellendus?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi necessitatibus, repudiandae, eius tempore deserunt ullam, maxime perferendis ad voluptatibus magnam minus omnis quidem hic at similique amet officiis quis quisquam?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere perferendis consectetur beatae, quod nobis dolor harum nemo impedit vitae adipisci corrupti illo itaque, aperiam incidunt. Temporibus blanditiis ea velit! Iure?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quo officiis odio officia ipsa dolores unde vitae praesentium consequuntur, earum provident reiciendis neque laboriosam quidem ratione blanditiis maxime autem. Distinctio!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus unde recusandae, iusto quaerat aliquam repellendus totam dolor ipsam earum quia, molestias dolores est quas nostrum maxime. Doloribus eum dolor repellendus?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi necessitatibus, repudiandae, eius tempore deserunt ullam, maxime perferendis ad voluptatibus magnam minus omnis quidem hic at similique amet officiis quis quisquam?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere perferendis consectetur beatae, quod nobis dolor harum nemo impedit vitae adipisci corrupti illo itaque, aperiam incidunt. Temporibus blanditiis ea velit! Iure?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quo officiis odio officia ipsa dolores unde vitae praesentium consequuntur, earum provident reiciendis neque laboriosam quidem ratione blanditiis maxime autem. Distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus unde recusandae, iusto quaerat aliquam repellendus totam dolor ipsam earum quia, molestias dolores est quas nostrum maxime. Doloribus eum dolor repellendus?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi necessitatibus, repudiandae, eius tempore deserunt ullam, maxime perferendis ad voluptatibus magnam minus omnis quidem hic at similique amet officiis quis quisquam?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere perferendis consectetur beatae, quod nobis dolor harum nemo impedit vitae adipisci corrupti illo itaque, aperiam incidunt. Temporibus blanditiis ea velit! Iure?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quo officiis odio officia ipsa dolores unde vitae praesentium consequuntur, earum provident reiciendis neque laboriosam quidem ratione blanditiis maxime autem. Distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus unde recusandae, iusto quaerat aliquam repellendus totam dolor ipsam earum quia, molestias dolores est quas nostrum maxime. Doloribus eum dolor repellendus?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi necessitatibus, repudiandae, eius tempore deserunt ullam, maxime perferendis ad voluptatibus magnam minus omnis quidem hic at similique amet officiis quis quisquam?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere perferendis consectetur beatae, quod nobis dolor harum nemo impedit vitae adipisci corrupti illo itaque, aperiam incidunt. Temporibus blanditiis ea velit! Iure?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quo officiis odio officia ipsa dolores unde vitae praesentium consequuntur, earum provident reiciendis neque laboriosam quidem ratione blanditiis maxime autem. Distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus unde recusandae, iusto quaerat aliquam repellendus totam dolor ipsam earum quia, molestias dolores est quas nostrum maxime. Doloribus eum dolor repellendus?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi necessitatibus, repudiandae, eius tempore deserunt ullam, maxime perferendis ad voluptatibus magnam minus omnis quidem hic at similique amet officiis quis quisquam?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere perferendis consectetur beatae, quod nobis dolor harum nemo impedit vitae adipisci corrupti illo itaque, aperiam incidunt. Temporibus blanditiis ea velit! Iure?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quo officiis odio officia ipsa dolores unde vitae praesentium consequuntur, earum provident reiciendis neque laboriosam quidem ratione blanditiis maxime autem. Distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus unde recusandae, iusto quaerat aliquam repellendus totam dolor ipsam earum quia, molestias dolores est quas nostrum maxime. Doloribus eum dolor repellendus?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi necessitatibus, repudiandae, eius tempore deserunt ullam, maxime perferendis ad voluptatibus magnam minus omnis quidem hic at similique amet officiis quis quisquam?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere perferendis consectetur beatae, quod nobis dolor harum nemo impedit vitae adipisci corrupti illo itaque, aperiam incidunt. Temporibus blanditiis ea velit! Iure?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quo officiis odio officia ipsa dolores unde vitae praesentium consequuntur, earum provident reiciendis neque laboriosam quidem ratione blanditiis maxime autem. Distinctio!
+    </div>
   )
 }
 
