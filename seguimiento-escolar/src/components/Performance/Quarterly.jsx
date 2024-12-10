1d034b6c21195f98b7da948b6beabc9658d9d5ef
Juan-Diego-E <juanelissalde2002@gmail.com>
2024-12-10 18:39:17 -0300

    Updating Charts

--- seguimiento-escolar/src/components/Performance/Quarterly.jsx
+++ seguimiento-escolar/src/components/Performance/Quarterly.jsx
@@ -1,29 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Performance.css';
-import { LineChart } from 'lucide-react';
+import ReactApexChart from 'react-apexcharts';
 
-const Quarterly = ({ data }) => {
-    const months = Object.keys(data);
-    const values = Object.values(data);
-    const maxValue = Math.max(...values);
+const Quarterly = () => {
+    const [chartOptions, setChartOptions] = useState({
+        chart: {
+            id: 'monthly-performance',
+            height: 350,
+            type: 'line',
+            toolbar: {
+                show: false
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            curve: 'smooth',
+            width: 10
+        },
+        fill: {
+            type: 'gradient',
+            gradient: {
+                shade: 'light',
+                type: 'vertical',
+                shadeIntensity: 0.25,
+                inverseColors: false,
+                opacityFrom: 0.7,
+                opacityTo: 0.3,
+                stops: [0, 100]
+            }
+        },
+        xaxis: {
+            categories: ['Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+            labels: {
+                style: {
+                    fontSize: '12px',
+                    colors: '#9c9c9c'
+                }
+            }
+        },
+        yaxis: {
+            min: 0,
+            max: 10,
+            labels: {
+                style: {
+                    fontSize: '12px',
+                    colors: '#9c9c9c'
+                }
+            }
+        },
+        tooltip: {
+            enabled: true
+        }
+    });
+
+    const [chartSeries, setChartSeries] = useState([
+        {
+            name: 'Rendimiento',
+            data: [0, 4, 6, 5, 4, 6, 4, 3, 6, 8, 5]
+        }
+    ]);
 
     return (
         <div className="annual-stats-container">
             <div className="flex items-center gap-2">
-                <LineChart className="icon" size={24} />
-                <h3>Rendimiento trimestral</h3>
+                <h3>Rendimiento Trimestral</h3>
             </div>
-            <ul className="monthly-performance-list">
-                {months.map((month, index) => (
-                    <li key={month} className="monthly-performance-item">
-                        <div
-                            className="progress-bar"
-                            style={{ height: `${(values[index] / maxValue) * 100}%` }}
-                        />
-                        <p>{month}</p>
-                    </li>
-                ))}
-            </ul>
+            <ReactApexChart
+                options={chartOptions}
+                series={chartSeries}
+                type="line"
+                height={350}
+            />
         </div>
     );
 };
--- seguimiento-escolar/src/components/Performance/Theme.jsx
+++ seguimiento-escolar/src/components/Performance/Theme.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 import './Performance.css';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, CartesianGrid, ResponsiveContainer } from 'recharts';
+
 
 const Theme = ({ themeData }) => {
-    const themes = Object.entries(themeData).map(([key, value]) => ({
-        unit: key,
-        value,
-    }));
+    const data = [
+        { name: 'U 1', unidad: 8 },
+        { name: 'U 2', unidad: 10 },
+        { name: 'U 3', unidad: 4 },
+        { name: 'U 4', unidad: 7 },
+        { name: 'U 5', unidad: 5 },
+        { name: 'U 6', unidad: 8 },
+    ];
 
     return (
         <div className="theme-container">
             <h3>Rendimiento por temas</h3>
-            <ul className="u-performance-theme">
-                {themes.map(({ unit, value }) => (
-                    <li key={unit} className="u-performance-bar">
-                        <div
-                            className="progress-bar"
-                            style={{ height: `${(value / 10) * 100}%` }}
-                        />
-                        <p>{unit}</p>
-                    </li>
-                ))}
-            </ul>
+            <ResponsiveContainer>
+            <BarChart width={600} height={300} data={data} margin={0} >
+                <XAxis dataKey="name" stroke="#3e75c9cb" />
+                <YAxis />
+                <Tooltip wrapperStyle={{ width: 100, backgroundColor: '#red' }} />
+                <Legend width={100} wrapperStyle={{ top: 4, right: 4, padding: '4px', backgroundColor: '#f5f5f5', border: '1px solid #d5d5d5', borderRadius: 3, lineHeight: '10px' }} />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <Bar dataKey="unidad" fill="#3e75c9cb" barSize={20} />
+            </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
